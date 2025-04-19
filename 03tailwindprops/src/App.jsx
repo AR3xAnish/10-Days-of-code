099144aea8caf61e6f5761168c49da539d3e034c
AR3xAnish <anishsnilajkar@mail.com>
2025-04-19 19:17:59 +0530

    Components reusability

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,14 @@
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Card pic="https://cdns.klimg.com/mav-prod-resized/480x/ori/feedImage/2025/3/19/1742352733648-rgazm.jpeg" content="When life give you tangerines" genre="Slice of life"/>
+      <Card content="Passengers" genre="Thriller"/>
+      <Card content="Attack on Titan" genre="Action"/>
+    </>
+  )
+}
+
+export default App
