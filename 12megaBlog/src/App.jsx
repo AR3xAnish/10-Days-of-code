311c0d70547bc48412599dbe35583fd7a6851029
AR3xAnish <anishsnilajkar@gmail.com>
2025-06-19 22:40:50 +0530

    configure redux toolkit

--- 12megaBlog/src/App.jsx
+++ 12megaBlog/src/App.jsx
@@ -1,13 +1,40 @@
+import {useDispatch} from 'react-redux'
+import { useEffect, useState } from 'react'
+import authServices from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
+import {Header,Footer} from "./components/index"
+import { Outlet } from 'react-router-dom'
 
-import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading,setLoading]=useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authServices.getCurrentUser()
+    .then((userData)=>{
+      if(userData) {
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout(userData))
+      }
+    })
+    .finally(()=>{setLoading(false)})
+  }, [])
+  
 
   return (
-    <>
-      <h1>blog in AppWrite</h1>
-    </>
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+
+        <Footer />
+      </div>
+    </div>
   )
 }
 
